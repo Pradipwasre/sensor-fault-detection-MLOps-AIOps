90f66160ad5149fbc5fe8447d3622fc548a880b2
pradipwasre <pradipwasre20@gmail.com>
2022-11-07 18:02:37 +0530

    updated files

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import pymongo
 from setuptools import find_packages,setup
 from typing import List
 
@@ -6,10 +7,7 @@ def get_requirements()->List[str]:
     This function will return list of requirements
     """
     requirement_list:List[str] = []
-
-    """
-    Write a code to read requirements.txt file and append each requirements in requirement_list variable.
-    """
+    ""
     return requirement_list
 
 setup(
